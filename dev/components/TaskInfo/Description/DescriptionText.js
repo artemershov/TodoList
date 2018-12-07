9f12ff9324711ce5953ce3cddc3bc1a14ab1abb9
John Smith <artemershov1@gmail.com>
2018-12-07 02:23:25 +0200

    fix

--- dev/components/TaskInfo/Description/DescriptionText.js
+++ dev/components/TaskInfo/Description/DescriptionText.js
@@ -3,7 +3,9 @@ import FormattedText from '../../shared/FormattedText';
 
 const DescriptionText = props => (
   <div className="px-3 mb-2">
-    <div className="mb-2">{<FormattedText text={props.data} />}</div>
+    <div className="mb-2">
+      <FormattedText text={props.data} />
+    </div>
     <a href="#" onClick={props.edit}>
       Редактировать
     </a>
